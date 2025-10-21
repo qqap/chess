0c511daf7ae7cb3666237579aebd34965a87cf41
qqap <lavahumour@gmail.com>
2025-10-21 22:38:03 +0530

    patch

--- public/getPossibleMoves.d.ts
+++
@@ -1,4 +0,0 @@
-import { ChessPiece, ChessBoard, Position } from './types.js';
-export declare function getPossibleMoves(board: ChessBoard, piece: ChessPiece, row: number, col: number, isDoubleMove?: boolean): Position[];
-export default getPossibleMoves;
-//# sourceMappingURL=getPossibleMoves.d.ts.map
